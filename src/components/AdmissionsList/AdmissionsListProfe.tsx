07aaedc113c9d71325afaf9b5800e0ec5b911759
mohamed <mohamedabdelwahidi@gmail.com>
2024-08-02 14:49:18 +0100

    I think good.

--- src/components/AdmissionsList/AdmissionsListProfe.tsx
+++ src/components/AdmissionsList/AdmissionsListProfe.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 //import axios from 'axios';
 //import withAuth from '@/hoc/withAuth';
 import Link from 'next/link';
-import { format } from 'date-fns';  // or import moment from 'moment';
+//import { format } from 'date-fns';  // or import moment from 'moment';
 
 
 
@@ -372,7 +372,8 @@ const AdmissionsListProfe: React.FC = () => {
                   <button className='bg-orange-400 hover:text-black ml-1 p-1 px-[14px] rounded-sm text-gray-900 font-[600]'>Ou<p className='inline ml-1'>Cours</p></button>
                   </Link>
                     ):(
-                      <p className='text-gray-300 font-[600]'>{format(new Date(form.date_interview), 'dd-MM-yyyy')}
+                      <p className='text-gray-300 font-[600]'>
+                        {/*{format(new Date(form.date_interview), 'dd-MM-yyyy')}*/}
                       
                       </p>
                     )}
