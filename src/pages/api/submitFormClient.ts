ee9d7e1cedd6199af3ceaf26e2fb114596c5a47a
mohamed <mohamedabdelwahidi@gmail.com>
2024-08-02 13:25:09 +0100

    again trying

--- src/pages/api/submitFormClient.ts
+++ src/pages/api/submitFormClient.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import connectDB from '../../../lib/db'
+import connectDB from '../lib/db'
 import AdmissionFormClient, { IAdmissionFormClient } from "../models/AdmissionFormClient";
 import bcrypt from "bcrypt";
 import nodemailer from "nodemailer";
